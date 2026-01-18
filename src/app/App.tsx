e2c5913b777cf707a180476e80931467ed742ad8
d4v3-rm <scarano.salvatore.71@gmail.com>
2026-01-18 10:46:26 +0100

    fix(layout): allow inspector pane to shrink with responsive limits

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,9 +14,9 @@ import { useThemeStore } from '@/store/themeStore'
 
 import styles from './App.module.scss'
 
-const MIN_LEFT_PANE_WIDTH = 220
-const MIN_RIGHT_PANE_WIDTH = 320
-const MIN_CENTER_PANE_WIDTH = 420
+const MIN_LEFT_PANE_WIDTH = 180
+const MIN_RIGHT_PANE_WIDTH = 220
+const MIN_CENTER_PANE_WIDTH = 300
 const TOTAL_SPLITTER_WIDTH = 20
 const MOBILE_BREAKPOINT = 1100
 
